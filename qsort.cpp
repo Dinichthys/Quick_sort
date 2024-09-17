a3afd7528c3d24f9f21a61079a74e598fc8feca9
Dinichthys <den2005425@gmail.com>
2024-09-17 18:14:22 +0300

    SHOK KONTENT

--- qsort.cpp
+++ qsort.cpp
@@ -11,8 +11,8 @@ int compare (const void* const a, const void* const b, const bool reverse)
     ASSERT(a != NULL, "Invalid argument for function my_strcmp\n");
     ASSERT(b != NULL, "Invalid argument for function my_strcmp\n");
 
-    const char* s1 = (const char*) a;
-    const char* s2 = (const char*) b;
+    const char* s1 = *((const char**) a);
+    const char* s2 = *((const char**) b);
 
     LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
 
@@ -44,24 +44,27 @@ void my_qsort(void *const base, const size_t nmemb, const size_t size,
     ASSERT(base   != NULL, "Invalid argument for function qsort\n");
     ASSERT(compar != NULL, "Invalid argument for function qsort\n");
 
-    char** s1  = (char**) base;
-    char** s2  = ((char**) base) + (nmemb - 1) * size;
-    char** mid = ((char**) base) + (nmemb - 1) / 2 * size;
+    char* s1  = (char*) base;
+    char* s2  = ((char*) base) + ((nmemb - 1) * size);
+    char* mid = ((char*) base) + ((nmemb - 1) / 2 * size);
 
+    size_t count = 0;
     do
     {
-        LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
+        LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n mid = %p\n"
+                           " *s1 = %p \n %lu\n",
+                           s1, s2, mid, *mid, count);
+        count++;
 
-        while (compar (*s1, *mid, reverse) < 0)
+        while (compar (s1, mid, reverse) < 0)
         {
-            LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
 
             s1 += size;
         }
 
         LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
 
-        while (compar (*s2, *mid, reverse) > 0)
+        while (compar (s2, mid, reverse) > 0)
         {
             s2 -= size;
         }
@@ -70,7 +73,7 @@ void my_qsort(void *const base, const size_t nmemb, const size_t size,
 
         for (size_t i = 0; i < size; i++)
         {
-            char* change = s1 [i];
+            char change = s1 [i];
                            s1 [i] = s2 [i];
                                     s2 [i] = change;
         }
@@ -82,13 +85,13 @@ void my_qsort(void *const base, const size_t nmemb, const size_t size,
     }
     while (s1 < s2);
 
-    if (s2 > (char**) base)
+    if (s2 > (char*) base)
     {
-        my_qsort (base, (s1 - (char**) base) / size, size, compar, reverse);
+        my_qsort (base, (s1 - (char*) base) / size, size, compar, reverse);
     }
 
-    if ((char**) base + (nmemb - 1) * size > s1)
+    if ((char*) base + (nmemb - 1) * size > s1)
     {
-        my_qsort (base, ((char**) base + (nmemb - 1) * size - s1) / size, size, compar, reverse);
+        my_qsort (base, ((char*) base + (nmemb - 1) * size - s1) / size, size, compar, reverse);
     }
 }
