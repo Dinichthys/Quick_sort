f5a524e7809f021e8c77417bb1f09368f84ce43b
Dinichthys <den2005425@gmail.com>
2024-09-20 22:29:25 +0300

    Add files via upload

--- qsort.cpp
+++ qsort.cpp
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <ctype.h>
+#include <stdint.h>
+#include <string.h>
 
 #include "qsort.h"
 #include "my_assert.h"
 #include "logging.h"
+#include "helpful.h"
 
 static void skip_non_abc (const char** const s, const int step);
+static void swap (void* const a, void* const b, const size_t size);
+static void vec_swap (void* a, void* b, const size_t size);
+static void memcpy_for_swap (void* const ptr1, void* const ptr2, void* const change, size_t size);
 
 static void skip_non_abc (const char** const s, const int step)
 {
@@ -26,7 +32,7 @@ int compare (const void* const a, const void* const b)
     const char* s1 = *((const char**) a);
     const char* s2 = *((const char**) b);
 
-//    LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
+    LOG(DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
 
     skip_non_abc (&s1, 1);
     skip_non_abc (&s2, 1);
@@ -51,7 +57,7 @@ int compare_r (const void* const a, const void* const b)
     const char* s1 = *((const char**) a);
     const char* s2 = *((const char**) b);
 
-//    LOG(stderr, DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
+    LOG(DEBUG, " s1 = %p \n s2 = %p\n", s1, s2);
 
     skip_non_abc (&s1, -1);
     skip_non_abc (&s2, -1);
@@ -74,45 +80,72 @@ void my_qsort(void *const base, const size_t nmemb, const size_t size,
     ASSERT(base   != NULL, "Invalid argument for function qsort\n");
     ASSERT(compar != NULL, "Invalid argument for function qsort\n");
 
+    LOG(INFO, " Parameters for function my_qsort:\n"
+              " base = %p \n nmemb = %lu\n size = %lu\n",
+              base, nmemb, size);
+
+    if (nmemb < 2)
+    {
+        return;
+    }
+
     char* left   = (char*) base;
-    char* right  = ((char*) base) + ((nmemb - 1) * size);
-    char* mid    = ((char*) base) + ((nmemb - 1) / 2 * size);
+    char* right  = (char*) base + ((nmemb - 1) * size);
+    char* mid    = (char*) base + (nmemb / 2 * size);
 
     size_t count = 1;
-    do
+    while (left < right)
     {
-        LOG(stderr, DEBUG, " left = %p \n right = %p\n mid = %p\n"
-                           " *mid = %p \n Iteration № %lu\n",
-                           left, right, mid, *mid, count);
+        LOG(DEBUG, " left = %p \n right = %p\n mid = %p\n"
+                   " Iteration № %lu\n",
+                   left, right, mid, count);
         count++;
 
-        while (compar (left, mid) < 0)
+        while ((left < right) && (compar (left, mid) < 0))
         {
             left += size;
         }
 
-        LOG(stderr, DEBUG, " left = %p \n right = %p\n", left, right);
+        LOG(DEBUG, " left = %p \n right = %p\n", left, right);
 
-        while (compar (right, mid) > 0)
+        while ((left < right) && (compar (right, mid) > 0))
         {
             right -= size;
         }
 
-        LOG(stderr, DEBUG, " left = %p \n right = %p\n size = %lu\n", left, right, size);
+        LOG(DEBUG, " left = %p \n right = %p\n size = %lu\n", left, right, size);
 
-        for (size_t i = 0; i < size; i++)
+        if (left < right)
         {
-            char change =  left [i];
-                           left [i] = right [i];
-                                      right [i] = change;
+            vec_swap (left, right, size);
+            /*
+            swap (left, right, size);
+            for (size_t i = 0; i < size; i++)
+            {
+                unsigned char change =  left [i];
+                                        left [i] = right [i];
+                                                   right [i] = change;
+            }
+            */
+
+            left  += size;
+            right -= size;
         }
 
-        left  += size;
-        right -= size;
-
-        LOG(stderr, DEBUG, " left = %p \n right = %p\n", left, right);
+        LOG(DEBUG, " left = %p \n right = %p\n", left, right);
     }
-    while (left < right);
+
+    if (left == right)
+    {
+        if (left > (char*) base)
+        {
+            right -= size;
+        }
+        else
+        {
+            left  += size;
+        }
+    }
 
     if (right > (char*) base)
     {
@@ -123,4 +156,85 @@ void my_qsort(void *const base, const size_t nmemb, const size_t size,
     {
         my_qsort (left, ((char*) base + (nmemb - 1) * size - left) / size + 1, size, compar);
     }
+}
+
+static void swap (void* const a, void* const b, const size_t size)
+{
+    ASSERT(a != NULL, "Invalid argument for function swap\n");
+    ASSERT(b != NULL, "Invalid argument for function swap\n");
+
+    LOG(DEBUG, " Parameters for function swap:\n"
+                       " a = %p \n b = %p\n size = %lu\n",
+                       a, b, size);
+
+    char* ptr_1 = (char*) a;
+    char* ptr_2 = (char*) b;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        char change =  ptr_1 [i];
+                       ptr_1 [i] = ptr_2 [i];
+                                   ptr_2 [i] = change;
+    }
+}
+
+static void vec_swap (void* a, void* b, size_t size)
+{
+    ASSERT(a != NULL, "Invalid argument for function vec_swap\n");
+    ASSERT(b != NULL, "Invalid argument for function vec_swap\n");
+
+    while (size > sizeof (uint64_t))
+    {
+        uint64_t change = 0;
+
+        memcpy_for_swap (a, b, &change, sizeof (uint64_t));
+
+        a = (uint8_t*) a + sizeof (uint64_t);
+        b = (uint8_t*) b + sizeof (uint64_t);
+        size -= sizeof (uint64_t);
+    }
+
+    if (size > sizeof (uint32_t))
+    {
+        uint32_t change = 0;
+
+        memcpy_for_swap (a, b, &change, sizeof (uint32_t));
+
+        a = (uint8_t*) a + sizeof (uint32_t);
+        b = (uint8_t*) b + sizeof (uint32_t);
+        size -= sizeof (uint32_t);
+    }
+
+    if (size > sizeof (uint16_t))
+    {
+        uint16_t change = 0;
+
+        memcpy_for_swap (a, b, &change, sizeof (uint16_t));
+
+        a = (uint8_t*) a + sizeof (uint16_t);
+        b = (uint8_t*) b + sizeof (uint16_t);
+        size -= sizeof (uint16_t);
+    }
+
+    if (size > sizeof (uint8_t))
+    {
+        uint8_t change = 0;
+
+        memcpy_for_swap (a, b, &change, sizeof (uint8_t));
+
+        a = (uint8_t*) a + sizeof (uint8_t);
+        b = (uint8_t*) b + sizeof (uint8_t);
+        size -= sizeof (uint8_t);
+    }
+}
+
+static void memcpy_for_swap (void* const ptr1, void* const ptr2, void* const change, const size_t size)
+{
+    ASSERT(ptr1   != NULL, "Invalid argument for function memcpy_for_swap\n");
+    ASSERT(ptr2   != NULL, "Invalid argument for function memcpy_for_swap\n");
+    ASSERT(change != NULL, "Invalid argument for function memcpy_for_swap\n");
+
+    memcpy (change, ptr1, size);
+            memcpy (ptr1, ptr2, size);
+                  memcpy (ptr2, change, size);
 }
